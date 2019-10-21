f272a6aac3e51abeb8a50b21488906d65eed9a8e
AreebSiddiqui <areeb40@hotmail.com>
2019-10-21 18:18:53 +0500

    chapter7

---
+++ chapter7/part5/resturant/target/debug/deps/resturant-d89d9426f8260464.d
@@ -0,0 +1,7 @@
+/home/areeb/c_project/chapter7/chapter7_5/resturant/target/debug/deps/libresturant-d89d9426f8260464.rlib: src/lib.rs src/front_house.rs src/front_house/hosting.rs
+
+/home/areeb/c_project/chapter7/chapter7_5/resturant/target/debug/deps/resturant-d89d9426f8260464.d: src/lib.rs src/front_house.rs src/front_house/hosting.rs
+
+src/lib.rs:
+src/front_house.rs:
+src/front_house/hosting.rs:
